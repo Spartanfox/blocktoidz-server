a40c96478d71d3ed0244dde3e1f16f366177fbf4
Ben <ben@localhost.localdomain>
2018-03-04 15:04:38 +1030

    Updated migrations

--- db/migrate/20180224232156_create_highscore.rb
+++ db/migrate/20180224232156_create_highscore.rb
@@ -2,8 +2,8 @@ class CreateHighscore < ActiveRecord::Migration[5.1]
   def change
     create_table :highscores do |t|
       t.string :name
-      t.string :date
-      t.string :score
+      t.string :time
+      t.integer :score
       t.references :level, foreign_key: true
       t.timestamps
     end
--- db/migrate/20180228020247_change_highscores_score.rb
+++
@@ -1,6 +0,0 @@
-class ChangeHighscoresScore < ActiveRecord::Migration[5.1]
-  def change
-      remove_column :highscores, :score
-      add_column    :highscores, :score, :integer
-  end
-end
