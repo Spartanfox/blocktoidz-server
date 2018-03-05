cdee736aca9caa095e38bd888df0a9ad87e024ac
Ben <ben@localhost.localdomain>
2018-03-05 12:27:18 +1030

    added pages

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,11 @@ Rails.application.routes.draw do
   end
 
   get 'static_pages/home'
+  get 'static_pages/download'
 
   root 'static_pages#home'
 
   get '/home', to: 'static_pages#home'
+  get '/download', to: 'static_pages#download'
 
 end
